dea43174a77a2a611bd0458810a15dfd2e1519a1
LauraKguez <35940942+LauraKguez@users.noreply.github.com>
2018-06-06 00:21:29 +0200

    Update Lancement_du_jeu.java

--- src/Lancement_du_jeu.java
+++ src/Lancement_du_jeu.java
@@ -2,6 +2,9 @@
 public class Lancement_du_jeu {
 	public static void main(String[] args) {
 		
+		Music musique1 = new Music("Musique1.wav");
+		musique1.loop(); //loop = en boucle
+		
 		Menu M = new Menu(1600,900);
 		M.AfficherMenu();
 		
